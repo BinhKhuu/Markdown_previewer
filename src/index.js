b3f7c1e573ab384e375f8fe28318edcb2b413520
Binh Khuu <binhkhuu90@gmail.com>
2017-11-20 18:30:10 +0800

    added styles and updated index.html

--- src/index.js
+++ src/index.js
@@ -2,68 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+class TextBox extends React.Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
-      </button>
+      //<div className='container-fluid'>
+        <div className='row background'>
+          <div className='col-6'>
+            <div className='float-right'>
+              <textarea id='markdown-box' rows='22' type='text' class='form-control'>default text here :</textarea>
+            </div>
+          </div>
+          <div className='col-6'>
+            <textarea id='preview-box' rows='22' type='text' class='form-control' disabled>preview text here :</textarea>
+          </div>
+        </div>
+      //</div>
     );
   }
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
-
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
 
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <TextBox />,
   document.getElementById('root')
 );
 
 
+/*
+class TextBox extends React.Component {
+  render() {
+    return (
+      //<div className='container-fluid'>
+        <div className='row background'>
+          <div className='col-6'>
+            <div className='row'>
+              <div className='offset-6' id='markdown-box'><textarea id='markdown-box' rows='22' type='text' class='form-control'>default text here :</textarea></div>
+            </div>
+          </div>
+          <div className='col-6'>
+            <div className='row'>
+              <div className='col-6 preview-box center-block' >preview</div>
+            </div>
+          </div>
+        </div>
+      //</div>
+    );
+  }
+}
+
+*/
